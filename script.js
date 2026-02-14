467fa5676aa7793ada2fb6ad45b816a120f165dc
ubg42 <redtomatotiger@gmail.com>
2026-02-15 07:08:46 +1100

    Add File

---
+++ script.js
@@ -0,0 +1,12 @@
+function openFullscreen(){
+    let game = document.querySelector(".game-iframe");
+	if (game.requestFullscreen) {
+	  game.requestFullscreen();
+	} else if (game.mozRequestFullScreen) { /* Firefox */
+	  game.mozRequestFullScreen();
+	} else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+	  game.webkitRequestFullscreen();
+	} else if (game.msRequestFullscreen) { /* IE/Edge */
+	  game.msRequestFullscreen();
+	}
+}
